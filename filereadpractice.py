04f4a4ee99866b6286466544e294d9c9b0d1af92
Ingwellm <michael_ingwell@hotmail.com>
2015-04-23 16:26:31 -0700

    More practice

--- filereadpractice.py
+++ filereadpractice.py
@@ -1,11 +1,17 @@
 #File open and check if Pallindrome
+# location = raw_input("Enter location of file ")
+
+# def is_pallindrome(x):
+	# f = open(location).read().split('\n')
+	# for lines in f:
+		# if lines == lines[::-1]:
+			# print "%s is a pallindrome" % lines
+		# else:
+			# print "%s is not a pallindrome" % lines
+# is_pallindrome(list)
+from collections import Counter
+
 location = raw_input("Enter location of file ")
 
-def is_pallindrome(x):
-	f = open(location).read().split('\n')
-	for lines in f:
-		if lines == lines[::-1]:
-			print "%s is a pallindrome" % lines
-		else:
-			print "%s is not a pallindrome" % lines
-is_pallindrome(list)			
+
+		

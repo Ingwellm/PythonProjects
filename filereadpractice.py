8bf15d1c84bc31676c7660fd2d5825bebe458c4b
Ingwellm <michael_ingwell@hotmail.com>
2015-04-24 13:28:51 -0700

    Added character frequency

--- filereadpractice.py
+++ filereadpractice.py
@@ -9,9 +9,22 @@
 		# else:
 			# print "%s is not a pallindrome" % lines
 # is_pallindrome(list)
-from collections import Counter
 
+
+from collections import Counter
+#Prompts users for location of file
 location = raw_input("Enter location of file ")
+
+
+def char_freq_table(x):
+	f = open(location).read().split('\n') #Opens file and removes nobreakspace from end of line
+	for chars in f:
+		a =  Counter(chars)
+		sor = sorted(a.keys()) #Sorts the list by keys
+		for key in sor:        #Iterates over keys and prints them out sorted
+			print (key, a[key]), #Comma prints horizontal, taking it away prints vertical
+								
+char_freq_table(list)
 
 
 		

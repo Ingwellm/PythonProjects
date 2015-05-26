e3760db887a37fb8b29c25fa0f3f54fb389d4099
Ingwellm <michael_ingwell@hotmail.com>
2015-05-25 23:35:39 -0700

    Cleaned up a bit

--- redditapi.py
+++ redditapi.py
@@ -1,29 +1,14 @@
 import praw
-import pprint
 
-
+#NEED TO DO:  Add list that can be updated and then ran against
 
 r = praw.Reddit(user_agent = "Top buildapcsales submissions by /u/topgearsad")
 submissions = r.get_subreddit('buildapcsales').get_new(limit=10)
 
-#Accesses object submissions property of '.title' which is the title of submission
+#Accesses object submissions property of '.title' which is the title of submission.  By default this is in unicode...so I forced it to string
 
 for sub in submissions:
 	if "[CASE]" in str(sub.title):
 		print sub.title
 
-
-	#for x in sub.title:
-		 #pprint.pprint(str(x))
-
-
-
-
-
-
-		
-	#if sub.title == '[HDD] 2x3TB Seagate Barracuda ST3000DM001 ($91.00 each + 10% off with code CCEX052215 = $163.80, free shipping)':
-		#print "Great Scott!"
-
-	#print thing.title - Used to test accessing object items?
-	#pprint.pprint(vars(thing)) - REALLY IMPORTANT!!!! Will print object items to access
+#pprint.pprint(vars(thing)) - REALLY IMPORTANT!!!! Will print object items to access

9e1848ac0436edbb99e156a517bd1e6a11a488a8
Ingwellm <michael_ingwell@hotmail.com>
2015-06-06 15:41:40 -0700

    Added simple logic to eight char length

--- redditapi.py
+++ redditapi.py
@@ -1,14 +1,19 @@
 import praw
 
 #NEED TO DO:  Add list that can be updated and then ran against
+look_for_these_deals = ['XB270HU'. '1440']
+title_to_look_at = submissions.sub.title #Is this correct?
 
 r = praw.Reddit(user_agent = "Top buildapcsales submissions by /u/topgearsad")
-submissions = r.get_subreddit('buildapcsales').get_new(limit=10)
+subreddit = r.get_subreddit('buildapcsales')
+submissions = subreddit.get_new(limit=10)
 
 #Accesses object submissions property of '.title' which is the title of submission.  By default this is in unicode...so I forced it to string
 
 for sub in submissions:
-	if "[CASE]" in str(sub.title):
+	if look_for_these_deals in str(sub.title):
 		print sub.title
+	else:
+		print "These are not the deals you are looking for"
 
 #pprint.pprint(vars(thing)) - REALLY IMPORTANT!!!! Will print object items to access
